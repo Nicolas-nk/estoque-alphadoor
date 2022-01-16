324b1a3745fcd6cd8f366b7770e65be92ba91203
Nicolas-nk <nicolas0102messias@gmail.com>
2022-01-16 17:44:20 -0300

    first commit

---
+++ api/dataBase/bd-estoque_alphadoor.sql
@@ -0,0 +1,12 @@
+create database bdestoque_alphadoor;
+use bdestoque_alphadoor;
+
+create table un_medida(
+	cod_un_medida int primary key auto_increment,
+    nome_un_medida varchar(50)
+);
+
+create table un_medida(
+	cod_un_medida int primary key auto_increment,
+    nome_un_medida varchar(50)
+);
